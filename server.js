ae456d31dfb25d54c0d64f6f6a27aa40072835d3
steph-harris <steph.harris21@gmail.com>
2016-02-23 16:06:27 -0500

    added bcrypt and encrypted registration passwords

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
+var bcrypt = require("bcryptjs");
 var session = require("express-session");
 var PORT = process.env.PORT || 8080;
 
 var Sequelize = require("sequelize");
 var sequelize = new Sequelize('my_class_db', 'root');
+
+var passport = require('passport');
+var passportLocal = require('passport-local');
 // create new user in db
-var User = sequelize.define('user', {
+var Instructor = sequelize.define('instructor', {
   firstname: {
     type: Sequelize.STRING,
     allowNull: false
@@ -28,8 +32,46 @@ var User = sequelize.define('user', {
   status: {
        type: Sequelize.STRING,
     allowNull: false
-}
+  }
+}, {
+  hooks: {
+    beforeCreate: function(input){
+      input.password = bcrypt.hashSync(input.password, 10);
+    }
+  }
 });
+
+var Student = sequelize.define('student', {
+  firstname: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastname: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  status: {
+       type: Sequelize.STRING,
+    allowNull: false
+  }
+}, {
+  hooks: {
+    beforeCreate: function(input){
+      input.password = bcrypt.hashSync(input.password, 10);
+    }
+  }
+});
+//Sequelize link instructor to students
+Instructor.hasMany(Student);
 
 var app = express();
 //get css,js, or images from files in public folder
@@ -42,6 +84,8 @@ app.use(session({
   saveUninitialized: true,
   resave: false
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
@@ -56,21 +100,30 @@ app.get("/", function(req, res){
 });
 
 app.post("/register", function(req,res){
-  User.create(req.body).then(function(result){
-   if(result.dataValues.status === "student"){
-      res.render("students", {result});
-    } else {
-      res.render("instructors");
-    }
-  }).catch(function(err) {
+  //create password hash
+  // })
+  //place new user in either student or instructor table
+  if(req.body.status === "student"){
+    Student.create(req.body).then(function(result){
+    res.render("students", {result});
+    }).catch(function(err) {
     console.log(err);
     res.redirect('/?msg=' + err.errors[0].message);
-  });
+    });
+  } else {
+    Instructor.create(req.body).then(function(result){
+    res.render("instructors", {result});
+    }).catch(function(err) {
+    console.log(err);
+    res.redirect('/?msg=' + err.errors[0].message);
+    });
+  }
 });
 
 app.get("/login", function(req, res){
   res.render("login");
 });
+
 //query the db to see if user is student or instructor and render correct page
 app.post("/login", function(req,res){
   User.findOne({ where: {email: req.body.email} }).then(function(result){
